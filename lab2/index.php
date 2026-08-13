b6b64d8857ce492b5c212e3f92f1c4674701c725
skie14 <kiel14.llabore@gmail.com>
2026-08-13 23:56:31 +0800

    update lab2

--- lab2/index.php
+++ lab2/index.php
@@ -1,17 +1,79 @@
+<?php
+$title = "About Me";
+$header = "Welcome to my Portfolio";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> <?php echo $title; ?></title>
+    <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="contact.php">Contact</a></li>
-        </ul>
+
+
+
+    <nav class="navbar">
+        <div class="nav-link">
+            <a href="index.php">Home</a>
+            <a href="page1.php">Skills and Hobbies</a>
+            <a href="page2.php">Projects and Goals</a>
+        </div>
     </nav>
+
+
+
+
+    <main class="about-container">
+
+        <div class="about-text">
+            <h1> <?php echo $header; ?></h1>
+
+            <p>
+                Hello! My name is Kiel Llabore. I am a student and
+                an aspiring web developer with a passion for creating interactive
+                and user-friendly websites.
+                This portfolio contains information about me, my skills,
+                hobbies, projects, and future goals.
+            </p>
+
+            <div class="socials">
+                <h2>Connect with me</h2>
+
+                <a href="https://facebook.com" target="_blank">
+                    <img class="icon"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2023_Facebook_icon.svg/960px-2023_Facebook_icon.svg.png"
+                        alt="Facebook">
+                </a>
+
+                <a href="https://linkedin.com" target="_blank">
+                    <img class="icon"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/3840px-LinkedIn_icon.svg.png"
+                        alt="LinkedIn">
+                </a>
+
+                <a href="https://github.com" target="_blank">
+                    <img class="icon"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/960px-Octicons-mark-github.svg.png"
+                        alt="GitHub">
+                </a>
+            </div>
+        </div>
+
+
+        <div class="about-image">
+            <img
+                src="assets/pfp.png"
+                alt="Profile Picture" width="200">
+        </div>
+
+    </main>
+
 </body>
+
 </html>
---
+++ lab2/page1.php
@@ -0,0 +1,95 @@
+<?php
+$title = "Skills and Hobbies";
+$header = "My Skills and Interests";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+    <nav class="navbar">
+        <div class="nav-link">
+            <a href="index.php">Home</a>
+            <a href="page1.php">Skills and Hobbies</a>
+            <a href="page2.php">Projects and Goals</a>
+        </div>
+    </nav>
+
+    <main class="skills-container">
+        <h1><?php echo $header; ?></h1>
+
+        <p class="intro">
+            I am currently learning HTML, CSS, PHP, and other programming
+            concepts. I enjoy creating simple websites and learning how
+            computers and technology work.
+        </p>
+
+        <section class="skills-section">
+            <h2>Technical Skills</h2>
+
+            <h3>Programming</h3>
+
+            <div class="skill-tags">
+                <span>C++</span>
+                <span>Java</span>
+                <span>PHP</span>
+                <span>C#</span>
+                <span>JavaScript</span>
+            </div>
+
+            <h3>Web Development</h3>
+
+            <div class="skill-tags">
+                <span>HTML</span>
+                <span>CSS</span>
+                <span>PHP</span>
+                <span>JavaScript</span>
+            </div>
+
+            <h3>Databases</h3>
+
+            <div class="skill-tags">
+                <span>MySql</span>
+            </div>
+
+            <h3>Game Development</h3>
+
+            <div class="skill-tags">
+                <span>Unity</span>
+                <span>C#</span>
+                <span>Aseprite</span>
+            </div>
+
+        </section>
+
+
+        <section class="hobbies-section">
+
+            <h2>Personal Interests</h2>
+
+            <div class="hobby-tags">
+                <span>Playing Video Games</span>
+                <span>Reading Novels</span>
+                <span>Listening to Music</span>
+            </div>
+            <div class="hobby-image">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrM9vaJpmE8SkF8jhmbq_gEU8qGT-fhc4r9M6JZoZRjBJ3ObUsDaZDwoEt&s=10"
+                    alt="Profile Picture" width="200">
+                <p>One of my favorite things to do is spend time with cats.</p>
+
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
